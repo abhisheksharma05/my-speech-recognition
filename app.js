9f100070b7ea1549aa8e6b29717e68acf89102b8
abhishek sharma <70977933+abhisheksharma05@users.noreply.github.com>
2020-11-30 11:46:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+let button = document.getElementById("btn");
+
+button.addEventListener("click", Speech);
+
+function Speech(){
+   window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+   var recognition = new SpeechRecognition();
+   recognition.interimResults = true;
+   let p = document.createElement('p');
+   const words  = document.querySelector('.words');
+   words.appendChild(p);
+   recognition.addEventListener('result' , (e)=>{
+           console.log(e.results);
+           const transcript = Array.from(e.results).map(result => result[0])
+           .map(result => result.transcript).join(" ");
+           p.textContent = transcript;
+   
+           if(e.results[0].isFinal){
+               p = document.createElement('p');
+               words.appendChild(p);
+           }
+          
+         
+   });
+   recognition.addEventListener('end' , recognition.start);
+   recognition.start();
+
+}
